6ce08fdccd63bef78d27498c6048a1d34250d71f
William <sr.w@hotmail.com>
2022-08-22 22:30:07 -0500

    e3

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -6,7 +6,24 @@ namespace Ejercicio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Funcion Factorial");
+            Console.WriteLine("Escribe un numero para calcular su factorial");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int f = factorial(num);
+            Console.WriteLine("El factorial del numero "+ num +" es: "+ f );
+        }
+        public static int factorial(int a)
+        { 
+            int contador = 1;
+            int factor = 1;
+
+            while(contador <= a)
+            {               
+                factor = factor * contador;
+                contador = contador + 1;                
+            }           
+
+            return factor;
         }
     }
 }
