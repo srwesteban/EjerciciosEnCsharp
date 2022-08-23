6184466219aa928a6da191de2a63a2f485cb5216
William <sr.w@hotmail.com>
2022-08-22 21:42:58 -0500

    e1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre");
+            string nombre = Console.ReadLine();
+            saludar(nombre);
+        }
+        public static void saludar(string a) => Console.WriteLine("Hola "+a);
+    }
+}
