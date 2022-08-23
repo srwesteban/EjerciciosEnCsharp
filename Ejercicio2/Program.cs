6acf22a132405ef8ba45b80b0f85512a83c74826
William <sr.w@hotmail.com>
2022-08-22 22:00:00 -0500

    e2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Funcion Suma\n");
+            Console.WriteLine("Escribe un numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe otro numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());     
+            int resultado= sumar(num, num2);
+            Console.WriteLine("La Suma de: " + num + " + " + num2 + " es: " + resultado); 
+        }
+        public static int sumar(int a, int b) => a + b;
+    }
+}
