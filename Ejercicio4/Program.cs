678cc2f5b301db996152fe0f9e0d97985ee60caa
William <sr.w@hotmail.com>
2022-08-23 22:40:53 -0500

    e4

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -12,6 +12,10 @@ namespace Ejercicio4
 
             int opcion = Convert.ToInt32(Console.ReadLine());
 
+            opc(opcion);
+        }
+        public static void opc(int opcion)
+        {
             switch (opcion)
             {
                 case 1:
@@ -21,51 +25,46 @@ namespace Ejercicio4
                     break;
                 case 2:
                     Console.WriteLine("Digite un numero");
-                    int num2 = Convert.ToInt32(Console.ReadLine());
-                    Binario(num2);
+                    long num2 = Convert.ToInt64(Console.ReadLine());                    
+                    long g = Binario(num2);
+                    Console.WriteLine(g);
                     break;
-            }       
+            }
         }
+
         public static void Decimal(int a)
-        {
-            
+        {            
             double x = 1;
             double binario = 0;
-            Console.WriteLine("El numero " + a + " convertido a binario es: ");
+            Console.WriteLine("El numero " + a + " convertido a Binario es: ");
             while (a >= 1)
             {
-
                 if (a % 2 == 1)
                 {
-
                     binario = binario + x;
                 }
                 a = Math.Abs(a / 2);
                 x = x * 10;
-
             }
             Console.WriteLine(binario);
-
         }
-        public static void Binario(int a)
-        {
-           
-            string binario = Convert.ToString(a);
-            int n = binario.Length;
+        public static int Binario(long a)
+        {        
+            int n =0;
             int x = 0;
+            const int div = 10;
             Console.WriteLine("El numero " + a + " convertido a Decimal es: ");
-            double deci = 0;
-            while (n > 0)
-            {                
-                if (binario.Substring(n) == "1")
-                {
-                    deci = deci + (Math.Pow(2,x));
-                }
-                n = n - 1;
-                x = x + 1;           
-            }
-            Console.WriteLine(deci);
 
+            for(long i = a, j = 0; i > 0; i/= div,j++)
+            {
+                x = (int)i % div;
+                if(x!= 1 && x != 0)
+                {
+                    Console.WriteLine("No se puede convertir");
+                }
+                n += x * (int)Math.Pow(2, j);
+            }
+            return n;        
         }
     }
 }
